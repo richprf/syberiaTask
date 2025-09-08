81a305b1eeea788876af33c28ad10752c032fe75
sina <poorprofessor2000@gmail.com>
2025-09-08 10:09:43 +0330

    add react/query

--- src/providers.tsx
+++ src/providers.tsx
@@ -2,6 +2,16 @@
 
 import { HeroUIProvider } from "@heroui/react";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const client = new QueryClient();
+
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <HeroUIProvider>{children}</HeroUIProvider>;
+  return (
+    <HeroUIProvider>
+      <QueryClientProvider client={client}>
+          {children}
+        </QueryClientProvider>
+    </HeroUIProvider>
+  );
 }
